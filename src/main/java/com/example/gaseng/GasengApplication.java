d799401efec5f022e2895b2eb851bc4768c7f374
jiwon <kiwijomn@gmail.com>
2023-10-03 15:14:55 +0900

    Init: 프로젝트 개발 환경 설정

---
+++ src/main/java/com/example/gaseng/GasengApplication.java
@@ -0,0 +1,13 @@
+package com.example.gaseng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GasengApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GasengApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gaseng/GasengApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gaseng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GasengApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
