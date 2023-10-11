0bc58e9af9abe7a5a9eb6a9c9231373ef094a581
jiwon <kiwijomn@gmail.com>
2023-10-12 01:33:34 +0900

    Feat: BaseTimeEntity와 BaseException 추가

---
+++ src/main/java/com/gaseng/global/exception/ErrorCode.java
@@ -0,0 +1,9 @@
+package com.gaseng.global.exception;
+
+import org.springframework.http.HttpStatus;
+
+public interface ErrorCode {
+    HttpStatus getStatus();
+    String getErrorCode();
+    String getMessage();
+}
