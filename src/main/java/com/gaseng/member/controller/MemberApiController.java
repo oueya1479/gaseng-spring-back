b97f2ed3bc1fe474799f6e808423fd0e54350803
jiwon <kiwijomn@gmail.com>
2023-10-19 02:29:57 +0900

    Docs: 회원가입 API 문서화

--- src/main/java/com/gaseng/member/controller/MemberApiController.java
+++ src/main/java/com/gaseng/member/controller/MemberApiController.java
@@ -6,6 +6,12 @@ import com.gaseng.member.dto.LoginRequest;
 import com.gaseng.member.dto.LoginResponse;
 import com.gaseng.member.dto.SignUpRequest;
 import com.gaseng.member.service.MemberService;
+import io.swagger.annotations.Api;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,12 +20,19 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
+@Api(tags = "Member Api")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/member")
 public class MemberApiController {
     private final MemberService memberService;
 
+    @Operation(summary = "회원가입", description = "사용자가 회원가입을 진행합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "회원가입 성공", content = @Content(schema = @Schema(implementation = BaseResponse.class))),
+            @ApiResponse(responseCode = "400", description = "이메일/비밀번호 형식이 올바르지 않습니다.", content = @Content(schema = @Schema(implementation = BaseResponse.class))),
+            @ApiResponse(responseCode = "409", description = "이미 등록된 이메일/전화번호/닉네임입니다.", content = @Content(schema = @Schema(implementation = BaseResponse.class)))
+    })
     @PostMapping("/sign-up")
     public BaseResponse<Long> signUp(@RequestBody @Valid SignUpRequest request) {
         return new BaseResponse<>(memberService.signUp(request.toMember()));
--- src/main/java/com/gaseng/member/dto/SignUpRequest.java
+++ src/main/java/com/gaseng/member/dto/SignUpRequest.java
@@ -5,28 +5,42 @@ import com.gaseng.member.domain.Email;
 import com.gaseng.member.domain.Member;
 import com.gaseng.member.domain.Password;
 import com.gaseng.member.domain.Sex;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 import static com.gaseng.member.domain.Password.ENCODER;
 
 public record SignUpRequest(
+        @Size(min = 5)
         @NotBlank(message = "이메일은 필수입니다.")
+        @Schema(description = "이메일", example = "example@gmail.com")
         String email,
 
+        @Size(min = 8, max = 12, message = "8자 이상, 12자 이하여야 합니다.")
         @NotBlank(message = "비밀번호는 필수입니다.")
+        @Schema(description = "비밀번호", example = "pass1234!")
         String password,
 
+        @Size(min = 2, message = "2글자 이상이어야 합니다.")
         @NotBlank(message = "이름은 필수입니다.")
+        @Schema(description = "이름", example = "홍길동")
         String name,
 
+        @Size(min = 2, message = "2글자 이상이어야 합니다.")
         @NotBlank(message = "닉네임은 필수입니다.")
+        @Schema(description = "닉네임", example = "길동이")
         String nickname,
 
+        @Size(message = "size에 메세지만 있어도 될까요?")
         @ValidEnum(enumClass = Sex.class)
+        @Schema(description = "성별", example = "0")
         Sex sex,
 
+        @Size(min = 10, max = 11, message = "숫자만 입력해주세요.")
         @NotBlank(message = "전화번호는 필수입니다.")
+        @Schema(description = "전화번호", example = "01012345678")
         String phone
 ) {
     public Member toMember() {
