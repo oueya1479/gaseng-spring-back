7c9cf34a7ab77f3a627005f23fd91ab0f893ed3e
jiwon <kiwijomn@gmail.com>
2023-10-19 15:25:04 +0900

    Docs: refresh token 재발급 API 문서화

--- src/main/java/com/gaseng/jwt/controller/TokenApiController.java
+++ src/main/java/com/gaseng/jwt/controller/TokenApiController.java
@@ -5,6 +5,9 @@ import com.gaseng.global.annotation.ExtractToken;
 import com.gaseng.global.common.BaseResponse;
 import com.gaseng.jwt.dto.TokenResponse;
 import com.gaseng.jwt.service.TokenReissueService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +19,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class TokenApiController {
     private final TokenReissueService tokenReissueService;
 
+    @Operation(summary = "refresh token 재발급", description = "refresh token을 재발급합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "요청에 성공했습니다."),
+            @ApiResponse(responseCode = "401", description = "유효하지 않은 토큰입니다."),
+            @ApiResponse(responseCode = "500", description = "서버와의 연결에 실패했습니다.")
+    })
     @PostMapping("/reissue")
     public BaseResponse<TokenResponse> reissueTokens(@ExtractPayload Long memId, @ExtractToken String refreshToken) {
         TokenResponse tokenResponse = tokenReissueService.reissueTokens(memId, refreshToken);
