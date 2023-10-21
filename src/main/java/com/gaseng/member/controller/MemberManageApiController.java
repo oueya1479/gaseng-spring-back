b834c6f8b077310a3f56344baf8b429fc9077c0e
jiwon <kiwijomn@gmail.com>
2023-10-21 14:28:15 +0900

    Docs: NORMAL 상태 변경 API 문서화

--- src/main/java/com/gaseng/member/controller/MemberManageApiController.java
+++ src/main/java/com/gaseng/member/controller/MemberManageApiController.java
@@ -3,6 +3,10 @@ package com.gaseng.member.controller;
 import com.gaseng.global.common.BaseResponse;
 import com.gaseng.member.service.MemberManageService;
 import io.swagger.annotations.Api;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,8 +20,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class MemberManageApiController {
     private final MemberManageService memberManageService;
 
+    @Operation(summary = "사용자 상태 NORMAL로 변경", description = "사용자의 상태를 NORMAL로 변경합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "요청에 성공했습니다."),
+            @ApiResponse(responseCode = "409", description = "요청과 동일한 상태입니다."),
+            @ApiResponse(responseCode = "500", description = "서버와의 연결에 실패했습니다.")
+    })
     @PatchMapping("/normal/{memId}")
-    public BaseResponse<Long> toNormal(@PathVariable Long memId) {
+    public BaseResponse<Long> toNormal(@Parameter(description = "memberId", required = true, example = "1") @PathVariable Long memId) {
         return new BaseResponse<>(memberManageService.toNormal(memId));
     }
 
