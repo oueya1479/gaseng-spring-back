ac6b68b09dfecc4052e21a78d7c54fa06d5de81c
jiwon <kiwijomn@gmail.com>
2023-10-22 18:05:29 +0900

    Feat: 인가를 위한 @EnableGlobalMethodSecurity 추가

--- src/main/java/com/gaseng/global/config/SecurityConfig.java
+++ src/main/java/com/gaseng/global/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import com.gaseng.global.security.util.JwtRequestFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
@@ -15,6 +16,11 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(
+        prePostEnabled = true,
+        securedEnabled = true,
+        jsr250Enabled = true
+)
 @RequiredArgsConstructor
 public class SecurityConfig {
     private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
