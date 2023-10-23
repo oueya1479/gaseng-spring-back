a2a4fde80556731c4dcda4fe2182479a43053af7
heo__ony <oueya1479@gmail.com>
2023-10-23 19:11:47 +0900

    Feat: sharehouse 조회 & 생성 & 수정 기능 추가

---
+++ src/main/java/com/gaseng/sharehouse/service/SharehouseImageService.java
@@ -0,0 +1,49 @@
+package com.gaseng.sharehouse.service;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.gaseng.file.domain.File;
+import com.gaseng.file.service.FileService;
+import com.gaseng.member.repository.MemberRepository;
+import com.gaseng.sharehouse.domain.Sharehouse;
+import com.gaseng.sharehouse.repository.SharehouseRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class SharehouseImageService {
+	private final FileService fileService;
+    private final S3Uploader s3Uploader;
+    
+    private static final String IMAGE_PATH = "sharehouse";
+
+	public void uploadS3Poster(MultipartFile f, Sharehouse sharehouse) throws IOException {
+    	String path = s3Uploader.upload(f, IMAGE_PATH);
+    	File file = File.builder()
+    			.member(sharehouse.getMember())
+    			.filePath(path)
+    			.build();
+    	
+    	sharehouse.updatePoster(path);
+    	fileService.saveFile(file);
+    }
+
+	public void uploadS3Images(List<MultipartFile> files, Sharehouse sharehouse) throws IOException {
+    	for (MultipartFile f : files) {
+    		String path = s3Uploader.upload(f, IMAGE_PATH);
+        	File file = File.builder()
+        			.member(sharehouse.getMember())
+        			.filePath(path)
+        			.build();
+        	fileService.saveFile(file, sharehouse);
+    	}
+    }
+    
+}
--- src/main/java/com/gaseng/sharehouse/service/SharehouseService.java
+++ src/main/java/com/gaseng/sharehouse/service/SharehouseService.java
@@ -1,19 +1,27 @@
 package com.gaseng.sharehouse.service;
 
-
+import com.gaseng.file.domain.ShareFile;
+import com.gaseng.global.exception.BaseException;
 import com.gaseng.member.domain.Member;
 import com.gaseng.member.repository.MemberRepository;
 import com.gaseng.sharehouse.domain.Sharehouse;
+import com.gaseng.sharehouse.domain.SharehouseStatus;
+import com.gaseng.sharehouse.dto.SharehouseRequest;
+import com.gaseng.sharehouse.dto.SharehouseResponse;
+import com.gaseng.sharehouse.dto.SharehouseUpdateRequest;
+import com.gaseng.sharehouse.exception.SharehouseErrorCode;
 import com.gaseng.sharehouse.repository.SharehouseRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+
+import javax.validation.Valid;
 
 @Service
 @Transactional
@@ -21,27 +29,61 @@ import java.util.Optional;
 public class SharehouseService {
     private final SharehouseRepository sharehouseRepository;
     private final MemberRepository memberRepository;
-    @Autowired
-    private S3Uploader s3Uploader;
-    public Long join(Long memId, MultipartFile file, Sharehouse sharehouse) throws IOException {
+    private final SharehouseImageService sharehouseImageService;
+    
+    public SharehouseResponse get(Long shrId) {
+    	Optional<Sharehouse> sharehouse = sharehouseRepository.findByShrId(shrId);
+    	Sharehouse sharehouseEntity = sharehouse.get();
+    	
+    	List<String> paths = new ArrayList<>();
+    	for (ShareFile shareFile : sharehouseEntity.getShareFiles()) {
+    		paths.add(shareFile.getFile().getFilePath());
+    	}
+    	
+		return new SharehouseResponse(
+				sharehouseEntity.getShrTitle(), 
+				sharehouseEntity.getShrDescription(), 
+				sharehouseEntity.getShrAddress(), 
+				sharehouseEntity.getShrAddressDetail(), 
+				sharehouseEntity.getShrPoster(), 
+				paths
+		);
+	}
+    
+    public Long create(Long memId, MultipartFile poster, SharehouseRequest request) throws IOException {
         Optional<Member> member = memberRepository.findByMemId(memId);
-        sharehouse = Sharehouse.builder()
-                .member(member.get())
-                .shrTitle(sharehouse.getShrTitle())
-                .shrDescription(sharehouse.getShrDescription())
-                .shrAddress(sharehouse.getShrAddress())
-                .shrAddressDetail(sharehouse.getShrAddressDetail())
-                .shrLatitude(sharehouse.getShrLatitude())
-                .shrLongitude(sharehouse.getShrLongitude())
-                .shrPoster(sharehouse.getShrPoster())
-                .shrStatus(sharehouse.getShrStatus())
+        
+        Sharehouse sharehouse = Sharehouse.builder()
+        		.member(member.get())
+                .shrTitle(request.shrTitle())
+                .shrDescription(request.shrDescription())
+                .shrAddress(request.shrAddress())
+                .shrAddressDetail(request.shrAddressDetail())
+                .shrPoster("")
+                .shrStatus(SharehouseStatus.ENABLE)
                 .build();
 
-
-        s3Uploader.upload(file,"file");
-        //file.setFilePath(storedFileName);
-        sharehouseRepository.save(sharehouse);
+        Sharehouse savedSharehouse = sharehouseRepository.save(sharehouse);
+        this.processImage(poster, request.files(), savedSharehouse);
+        
         return sharehouse.getShrId();
+    }
+    
+    public Long update(Long memId, SharehouseUpdateRequest request) {
+    	Optional<Sharehouse> sharehouse = sharehouseRepository.findByShrId(request.id());
+    	Sharehouse sharehouseEntity = sharehouse.get();
+    	
+    	this.isAuthor(memId, sharehouseEntity.getMember().getMemId());
+    	
+    	sharehouseEntity.updateTitleandDescription(request.title(), request.description());
+    	
+		return sharehouseEntity.getShrId();
+	}
+    
+    private void isAuthor(Long memId, Long author) {
+    	if (memId != author) {
+    		throw BaseException.type(SharehouseErrorCode.USER_ID_MISMATCH);
+    	}
     }
 
     public void updateChecklist(Long memId,Sharehouse updateSharehouse) {
@@ -50,6 +92,14 @@ public class SharehouseService {
         sharehouse.update(updateSharehouse);
         sharehouseRepository.save(sharehouse);
     }
-
-
+    
+    private void processImage(
+    		MultipartFile poster, 
+    		List<MultipartFile> files,
+    		Sharehouse sharehouse
+    ) throws IOException {
+    	sharehouseImageService.uploadS3Poster(poster, sharehouse);
+    	sharehouseImageService.uploadS3Images(files, sharehouse);
+    }
+    
 }
