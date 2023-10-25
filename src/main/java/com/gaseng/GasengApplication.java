f16c60a037b992e0d81e06f0e7720c1bc1638691
heo__ony <oueya1479@gmail.com>
2023-10-26 01:13:48 +0900

    Fix: 표준 시간대 UTC 변경

--- src/main/java/com/gaseng/GasengApplication.java
+++ src/main/java/com/gaseng/GasengApplication.java
@@ -1,10 +1,19 @@
 package com.gaseng;
 
+import java.util.TimeZone;
+
+import javax.annotation.PostConstruct;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GasengApplication {
+	
+	@PostConstruct
+    public void started() {
+      TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(GasengApplication.class, args);
