59cda8f33208658d18df5cd78be3a80f23da264d
heo__ony <oueya1479@gmail.com>
2023-11-01 16:50:10 +0900

    Fix: 오탈자 수정

--- src/main/java/com/gaseng/sharehouse/dto/SharehouseListResponse.java
+++ src/main/java/com/gaseng/sharehouse/dto/SharehouseListResponse.java
@@ -6,7 +6,7 @@ public record SharehouseListResponse (
         Long shrId,
         String shrTitle,
         String shrDescription,
-        String ShrAddress,
+        String shrAddress,
         String shrPoster
 
 ) {
