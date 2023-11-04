4d6db8c56f0e70b1d185e67c34d271aed758bfce
jiwon <kiwijomn@gmail.com>
2023-11-05 02:35:11 +0900

    Docs: Chat Api 문서화

--- src/main/java/com/gaseng/chat/controller/ChatRoomApiController.java
+++ src/main/java/com/gaseng/chat/controller/ChatRoomApiController.java
@@ -43,7 +43,6 @@ public class ChatRoomApiController {
     @Operation(summary = "채팅방 접속", description = "사용자가 채팅방에 접속하여 채팅을 조회합니다.")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "요청에 성공했습니다."),
-            @ApiResponse(responseCode = "400", description = "채팅이 불가능한 채팅방입니다."),
             @ApiResponse(responseCode = "404", description = "채팅방을 찾을 수 없습니다."),
             @ApiResponse(responseCode = "500", description = "서버와의 연결에 실패했습니다.")
     })
@@ -54,22 +53,48 @@ public class ChatRoomApiController {
     }
 
     @PreAuthorize("hasRole('USER')")
+    @Operation(summary = "채팅 메세지 등록", description = "채팅방의 마지막 메세지를 업데이트합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "요청에 성공했습니다."),
+            @ApiResponse(responseCode = "400", description = "채팅이 불가능한 채팅방입니다."),
+            @ApiResponse(responseCode = "404", description = "채팅방을 찾을 수 없습니다."),
+            @ApiResponse(responseCode = "500", description = "서버와의 연결에 실패했습니다.")
+    })
     @PatchMapping("/{chatRoomId}")
-    public BaseResponse<Long> updateMessage(@PathVariable Long chatRoomId, @RequestBody String message) {
+    public BaseResponse<Long> updateMessage(@Parameter(description = "chatroom id", required = true, example = "1")
+                                            @PathVariable Long chatRoomId,
+                                            @Parameter(description = "message content", required = true, example = "hello")
+                                            @RequestBody String message) {
         return new BaseResponse<>(chatRoomService.updateMessage(chatRoomId, message));
     }
 
     @PreAuthorize("hasRole('USER')")
+    @Operation(summary = "채팅방 전체 조회", description = "사용자의 전체 채팅방 목록을 조회합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "요청에 성공했습니다."),
+            @ApiResponse(responseCode = "404", description = "회원을 찾을 수 없습니다."),
+            @ApiResponse(responseCode = "500", description = "서버와의 연결에 실패했습니다.")
+    })
     @GetMapping("")
     public BaseResponse<List<ChatRoomListResponse>> getChatRoomList(@ExtractPayload Long memId,
+                                                                    @Parameter(description = "size of page", required = true, example = "10")
                                                                     @RequestParam(value = "page") int pageSize,
+                                                                    @Parameter(description = "last chatroom id in the list (default = -1)", example = "10")
                                                                     @RequestParam(value = "index", defaultValue = "-1", required = false) Long lastChatRoomId) {
         return new BaseResponse<>(chatRoomService.getChatRoomList(memId, pageSize, lastChatRoomId));
     }
 
     @PreAuthorize("hasRole('USER')")
+    @Operation(summary = "채팅방 전체 조회", description = "사용자의 전체 채팅방 목록을 조회합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "요청에 성공했습니다."),
+            @ApiResponse(responseCode = "404", description = "채팅방을 찾을 수 없습니다."),
+            @ApiResponse(responseCode = "500", description = "서버와의 연결에 실패했습니다.")
+    })
     @DeleteMapping("/{chatRoomId}")
-    public BaseResponse<Long> deleteChatRoom(@ExtractPayload Long memId, @PathVariable Long chatRoomId) {
+    public BaseResponse<Long> deleteChatRoom(@ExtractPayload Long memId,
+                                             @Parameter(description = "chatroom id", required = true, example = "1")
+                                             @PathVariable Long chatRoomId) {
         return new BaseResponse<>(chatRoomService.deleteChatRoom(memId, chatRoomId));
     }
 }
--- src/main/java/com/gaseng/chat/dto/ChatRoomListResponse.java
+++ src/main/java/com/gaseng/chat/dto/ChatRoomListResponse.java
@@ -1,9 +1,19 @@
 package com.gaseng.chat.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 public record ChatRoomListResponse(
+        @Schema(description = "chat_room id (index)", example = "1")
         Long chatRoomId,
+
+        @Schema(description = "chat partner nickname", example = "상대방 닉네임")
         String partnerNickname,
+
+        @Schema(description = "last message content", example = "hello")
         String message,
+
+        @Schema(description = "last message update time",
+                example = "지금/n분 전/n시간 전/n일 전/n달 전/n년 전")
         String updatedTime
 ) {
 }
