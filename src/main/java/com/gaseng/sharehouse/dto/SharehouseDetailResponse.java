247602d3c13374231b18883990919df96c6540b5
heo__ony <oueya1479@gmail.com>
2023-11-06 18:56:37 +0900

    Refactor: 쉐어하우스 조회 관련 response  리펙토링

--- src/main/java/com/gaseng/sharehouse/dto/SharehouseDetailResponse.java
+++ src/main/java/com/gaseng/sharehouse/dto/SharehouseDetailResponse.java
@@ -1,11 +1,21 @@
 package com.gaseng.sharehouse.dto;
 
+import java.util.Date;
 import java.util.List;
+
+import com.gaseng.checklist.domain.CheckCigarette;
+import com.gaseng.checklist.domain.CheckSleepingHabit;
+import com.gaseng.checklist.domain.CheckType;
 
 public record SharehouseDetailResponse (
         Long id,
-        Long memId,
-        Long checkId,
+        String name,
+        CheckCigarette cigarette,
+        CheckSleepingHabit habit,
+        CheckType type,
+        String mbti,
+        String place,
+        Date sleepTime,
         String title,
         String description,
         String address,
