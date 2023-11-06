3a3c7231ce8d9ebfd625d5323924a6f076099556
jiwon <kiwijomn@gmail.com>
2023-11-06 23:12:46 +0900

    Feat: 스크랩 게시물 API 페이지네이션 적용

--- src/main/java/com/gaseng/sharehouse/controller/SharehouseScrapApiController.java
+++ src/main/java/com/gaseng/sharehouse/controller/SharehouseScrapApiController.java
@@ -2,15 +2,18 @@ package com.gaseng.sharehouse.controller;
 
 import com.gaseng.global.annotation.ExtractPayload;
 import com.gaseng.global.common.BaseResponse;
-import com.gaseng.member.dto.MemberListResponse;
-import com.gaseng.sharehouse.dto.*;
+import com.gaseng.sharehouse.dto.SharehouseListResponse;
 import com.gaseng.sharehouse.service.ScrapService;
-import com.gaseng.sharehouse.service.SharehouseService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 import java.util.List;
 
 @ResponseBody
@@ -20,11 +23,16 @@ import java.util.List;
 public class SharehouseScrapApiController {
     private final ScrapService scrapService;
 
-    @Operation(summary = "내가 스크랩한 글 조회", description = "사용자가 스크랩한 글 전체 조회합니다.")
-    @GetMapping(value = "/all")
+    @PreAuthorize("hasRole('USER')")
+    @Operation(summary = "내가 스크랩한 글 조회", description = "사용자가 스크랩한 글을 전체 조회합니다.")
+    @GetMapping(value = "")
     public BaseResponse<List<SharehouseListResponse>> getAll(
-            @ExtractPayload Long memId
+            @ExtractPayload Long memId,
+            @Parameter(description = "size of page", required = true, example = "10")
+            @RequestParam(value = "page") int pageSize,
+            @Parameter(description = "last scrap id in the list (default = -1)", example = "10")
+            @RequestParam(value = "index", defaultValue = "-1", required = false) Long lastScrapId
     ) {
-        return new BaseResponse<>(scrapService.getScrapAll(memId));
+        return new BaseResponse<>(scrapService.getAll(memId, pageSize, lastScrapId));
     }
 }
--- src/main/java/com/gaseng/sharehouse/service/ScrapService.java
+++ src/main/java/com/gaseng/sharehouse/service/ScrapService.java
@@ -1,7 +1,7 @@
 package com.gaseng.sharehouse.service;
 
 import com.gaseng.member.domain.Member;
-import com.gaseng.member.repository.MemberRepository;
+import com.gaseng.member.service.MemberFindService;
 import com.gaseng.sharehouse.domain.Scrap;
 import com.gaseng.sharehouse.domain.Sharehouse;
 import com.gaseng.sharehouse.dto.SharehouseListResponse;
@@ -17,20 +17,22 @@ import java.util.List;
 @Transactional
 @RequiredArgsConstructor
 public class ScrapService {
-
-    private final MemberRepository memberRepository;
     private final ScrapRepository scrapRepository;
+    private final MemberFindService memberFindService;
 
-    public List<SharehouseListResponse> getScrapAll(Long memId) {
-        List<SharehouseListResponse> responses = new ArrayList<>();
-        Member member = memberRepository.findByMemId(memId).get();
+    @Transactional(readOnly = true)
+    public List<SharehouseListResponse> getAll(Long memId, int pageSize, Long lastShrId) {
+        Member member = memberFindService.findByMemId(memId);
         List<Scrap> scraps = scrapRepository.findByMemberOrderByScrapIdDesc(member);
+
         List<Sharehouse> sharehouses = new ArrayList<>();
         for (Scrap scrap : scraps) {
-                sharehouses.add(scrap.getSharehouse());
+            sharehouses.add(scrap.getSharehouse());
         }
-        for(Sharehouse sharehouse: sharehouses){
-            responses.add(new SharehouseListResponse(
+
+        List<SharehouseListResponse> scrapLists = new ArrayList<>();
+        for (Sharehouse sharehouse : sharehouses) {
+            scrapLists.add(new SharehouseListResponse(
                     sharehouse.getShrId(),
                     sharehouse.getShrTitle(),
                     sharehouse.getShrDescription(),
@@ -39,8 +41,24 @@ public class ScrapService {
             ));
         }
 
-        return responses;
+        int lastIndex = getLastIndex(scrapLists, lastShrId);
+
+        return getScrapResponse(scrapLists, lastIndex, pageSize);
     }
 
+    private int getLastIndex(List<SharehouseListResponse> scrapLists, Long lastShrId) {
+        return scrapLists.indexOf(
+                scrapLists.stream()
+                        .filter(scrap -> scrap.shrId().equals(lastShrId))
+                        .findFirst()
+                        .orElse(null)
+        );
+    }
 
+    private List<SharehouseListResponse> getScrapResponse(List<SharehouseListResponse> scrapLists, int lastIndex, int size) {
+        if (lastIndex + 1 + size >= scrapLists.size()) {
+            return (scrapLists.subList(lastIndex + 1, scrapLists.size()));
+        }
+        return (scrapLists.subList(lastIndex + 1, lastIndex + 1 + size));
+    }
 }
