264a29d6d3c579efd4015600d67c2037f693d0b0
jiwon <kiwijomn@gmail.com>
2023-11-10 17:24:41 +0900

    Feat: KycManageApi 인가 설정

--- src/main/java/com/gaseng/kyc/controller/KycManageApiController.java
+++ src/main/java/com/gaseng/kyc/controller/KycManageApiController.java
@@ -9,6 +9,7 @@ import io.swagger.annotations.Api;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -20,25 +21,29 @@ import java.util.List;
 @RequestMapping("/kyc")
 public class KycManageApiController {
 	private final KycManageService kycManageService;
-	
+
+	@PreAuthorize("hasRole('ADMIN')")
 	@Operation(summary = "kyc 요청 조회", description = "kyc 요청서를 조회합니다.")
 	@GetMapping("")
 	public BaseResponse<KycRequireResponse> get(@RequestParam Long kycrId) {
 		return new BaseResponse<>(kycManageService.get(kycrId));
 	}
-	
+
+	@PreAuthorize("hasRole('ADMIN')")
 	@Operation(summary = "kyc 요청 전체 조회", description = "kyc 요청서를 조회합니다.")
 	@GetMapping("/all")
 	public BaseResponse<List<KycRequireSummaryResponse>> getAll() {
 		return new BaseResponse<>(kycManageService.getAll());
 	}
-	
+
+	@PreAuthorize("hasRole('ADMIN')")
 	@Operation(summary = "kyc 저장", description = "kyc를 저장합니다.")
 	@PostMapping("")
 	public BaseResponse<Long> save(@RequestParam Long kycrId, @RequestBody @Valid KycSaveRequest request) throws Exception {
 		return new BaseResponse<>(kycManageService.save(kycrId, request));
 	}
 
+	@PreAuthorize("hasRole('ADMIN')")
 	@Operation(summary = "범죄 이력 조회", description = "kyc 인증을 요청한 사용자의 범죄 이력을 조회합니다.")
 	@GetMapping("/criminal-records/{kycrId}")
 	public BaseResponse<String> getCriminalRecord(@PathVariable Long kycrId) {
